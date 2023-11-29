3148c2037418111f2f98025d2351d1648615866b
Gonzalo Palma <gonzalopalmadev0210@gmail.com>
2023-11-29 18:11:43 -0500

    proyecto terminado

--- script.js
+++ script.js
@@ -11,18 +11,65 @@ const error_year = document.querySelector('.error-year')
 
 const fechaActual = new Date()
 
-input_day.addEventListener('keydown', (event)=>{
+input_day.addEventListener('input', ()=>{
+  validarDia()
+})
+
+function validarDia() {
   const value_day = Number(input_day.value)
   const value_month = Number(input_month.value)
   const value_year = Number(input_year.value)
-  
+  // Resetear clases de error y borde al inicio
+  input_day.classList.remove('border-error');
+  error_day.classList.add('error-day-none');
+
+  const par = value_month % 2 == 0
+  if (par) {
+    if (value_day > 30) {
+      input_day.classList.add('border-error')
+      error_day.classList.remove('error-day-none')
+    }  
+  } else if (!par) {
+    if (value_day > 31) {
+      input_day.classList.add('border-error')
+      error_day.classList.remove('error-day-none')
+    }
+  }
+}
+
+input_month.addEventListener('input', ()=>{
+  const value_day = Number(input_day.value)
+  const value_month = Number(input_month.value)
+  const value_year = Number(input_year.value)
+
+  input_month.classList.remove('border-error')
+  error_month.classList.add('error-month-none')
+
+  if (value_month > 12 || value_month < 0) {
+    input_month.classList.add('border-error')
+    error_month.classList.remove('error-month-none')
+  }
+  validarDia()
+})
+
+input_year.addEventListener('input', ()=>{
+  const value_day = Number(input_day.value)
+  const value_month = Number(input_month.value)
+  const value_year = Number(input_year.value)
+
+  input_year.classList.remove('border-error')
+  error_year.classList.add('error-year-none')  
+
+  if (value_year > fechaActual.getFullYear()) {
+    input_year.classList.add('border-error')
+    error_year.classList.remove('error-year-none')
+  }
 })
 
 icono_boton.addEventListener('click', ()=>{
   const value_day = Number(input_day.value)
   const value_month = Number(input_month.value)
   const value_year = Number(input_year.value)
-
   let edad = fechaActual.getFullYear() - value_year
   let meses = (fechaActual.getMonth() + 1) - value_month
   let dias = fechaActual.getDate() - value_day
